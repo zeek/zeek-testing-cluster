fb9cc9ee9989473a40738c5f4f4e0db6e50948c0
Christian Kreibich <christian@corelight.com>
2022-08-31 16:59:03 -0700

    Update reachability-controller test to reflect websocket port

--- tests/reachability-controller.sh
+++ tests/reachability-controller.sh
@@ -1,5 +1,5 @@
-# This test verifies that by default the controller listens globally on port
-# 2150.
+# This test verifies that by default the controller listens globally on ports
+# 2149 (websockets) and 2150 (Broker).
 
 # @TEST-REQUIRES: $SCRIPTS/docker-requirements
 # @TEST-EXEC: bash %INPUT
@@ -16,10 +16,12 @@ ZEEK_ENTRYPOINT=controller.zeek docker_compose_up
 # This needs "netstat":
 controller_cmd "apt-get -q update && apt-get install -q -y --no-install-recommends net-tools"
 
-# Wait a bit until the service is available from within the container.
+# Wait a bit until the services are available from within the container.
+try_until controller_cmd "exec 3<>/dev/tcp/127.0.0.1/2149" \
+    || fail "controller never offered websocket port"
 try_until controller_cmd "exec 3<>/dev/tcp/127.0.0.1/2150" \
-    || fail "controller never booted"
+    || fail "controller never offered Broker port"
 
-# The controller should be listening on 0.0.0.0:2150 now. The Supervisor should
-# *not* be listening.
+# The controller should be listening on 0.0.0.0:2149 and 0.0.0.0:2150 now.
+# The Supervisor should *not* be listening.
 controller_cmd "netstat -tpln | grep zeek | awk '{ print \$4 }' | sort" >output
