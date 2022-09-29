4edd9e8a06fc8193e458abfec1afb082d54e3529
Christian Kreibich <christian@corelight.com>
2022-09-28 22:37:43 -0700

    Add variants of the deploy-a2c test to cover (absence of) TLS
    This moves the gist of the test to a separate file, sourced from three
    tests: (1) the original deploy-a2c, (2) a variant that enables TLS entirely, (3)
    a variant that configures TLS authentication for controller and zeek-client.

---
+++ tests/deploy-a2c-authenticated.sh
@@ -0,0 +1,45 @@
+# This test verifies successful deployment of a config that specifies
+# agent-to-controller connectivity, with authenticated TLS.
+
+# @TEST-REQUIRES: $SCRIPTS/docker-requirements
+# @TEST-EXEC: bash %INPUT
+# @TEST-EXEC: btest-diff output.json
+# @TEST-EXEC: btest-diff output.ini
+
+. $SCRIPTS/docker-setup
+. $SCRIPTS/test-helpers
+
+docker_populate singlehost
+
+# Place the credentials into the local etc folder so they're available inside
+# the Docker containers:
+cp -pr $FILES/certs etc/
+
+cat >>zeekscripts/local.zeek <<EOF
+redef Broker::ssl_cafile = "/usr/local/etc/certs/ca.pem";
+redef Broker::ssl_certificate = "/usr/local/etc/certs/cert.1.pem";
+redef Broker::ssl_keyfile = "/usr/local/etc/certs/key.1.enc.pem";
+redef Broker::ssl_passphrase = "12345";
+EOF
+
+cat >>etc/zeek-client.cfg <<EOF
+[ssl]
+cafile = /usr/local/etc/certs/ca.pem
+certificate = /usr/local/etc/certs/cert.1.pem
+keyfile = /usr/local/etc/certs/key.1.enc.pem
+passphrase = 12345
+EOF
+
+docker_compose_up
+
+# The certificate below uses common name 1.foo.bar, so make the controller
+# available under that name. There are several ways for doing this,
+# including at the Docker level, but it seems easiest to just override name
+# resolution within the client container:
+client_cmd "apt-get -q update && apt-get install -q -y --no-install-recommends host"
+client_cmd 'echo "$(host controller | awk "{ print \$NF }") 1.foo.bar" >>/etc/hosts'
+
+# Our zeek_client wrapper honors this:
+export TEST_ZEEK_CLIENT_ARGS="--set controller.host=1.foo.bar"
+
+. $TEST_BASE/tests/deploy-a2c-common.sh
